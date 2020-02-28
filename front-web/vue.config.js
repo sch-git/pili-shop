7c271a9723a47d3beac2f79835bff288340272a7
sch <1208579668@qq.com>
2020-02-28 13:49:00 +0800

    DONE 搭建项目结构

---
+++ front-web/vue.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const resolve = dir => {
+  return path.join(__dirname, dir)
+}
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/pili-shop/' : '/'
+module.exports = {
+  devServer: {
+    port: 10090,
+    proxy: 'http://localhost:8100'
+  },
+  lintOnSave: false,
+  publicPath: BASE_URL,
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('_c', resolve('src/components'))
+  },
+  // 打包时不生成.map文件
+  productionSourceMap: false
+}
