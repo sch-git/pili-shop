88c0e18c0d3eae9692db158f3bf88a017a6c72e0
sch <1208579668@qq.com>
2020-03-18 23:17:39 +0800

    DONE 封装MessageBox-确认消息弹窗

--- front-web/src/lib/tools.js
+++ front-web/src/lib/tools.js
@@ -1,6 +1,24 @@
 import Vue from 'vue'
 import VueLazyLoad from 'vue-lazyload'
+import { MessageBox, Message } from 'element-ui'
+
+// 轮播图
 Vue.use(VueLazyLoad, {
   loading: '../img/loading_img.svg'
 })
 export default VueLazyLoad
+
+// 确认消息弹窗
+export const MessageBoxConfirm = (message, title) => {
+  return MessageBox.confirm(message, title, {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'error'
+  }).then(() => {
+    Message.success('删除成功')
+    return true
+  }).catch(() => {
+    Message.info('已取消删除')
+    return false
+  })
+}
