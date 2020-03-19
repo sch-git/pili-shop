e4912fff3c30b2db8e2bc3d67e0f08f828272d49
sch <1208579668@qq.com>
2020-03-19 13:19:01 +0800

    DONE 修改Jedis

---
+++ api/back-web/src/main/java/com/sch/backweb/config/JedisConfig.java
@@ -0,0 +1,47 @@
+package com.sch.backweb.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * @Description: JedisConfig
+ * @Author: chenghao.su
+ * @Date: 2020/3/17 13:45
+ */
+@Configuration
+public class JedisConfig extends CachingConfigurerSupport {
+    private Logger LOGGER = LoggerFactory.getLogger(JedisConfig.class);
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private int port;
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+    @Value("${spring.redis.password}")
+    private String password;
+    @Value("${spring.redis.jedis.pool.max-idle}")
+    private int maxIdle;
+    @Value("${spring.redis.jedis.pool.max-wait}")
+    private int maxWaitMills;
+
+    @Bean
+    public JedisPool redisPoolFactory() throws Exception {
+        LOGGER.info("Jedis注入成功! Redis地址：{}:{}", host, port);
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMaxWaitMillis(maxWaitMills);
+        // 连接耗尽时是否阻塞, false报异常,ture阻塞直到超时, 默认true
+        jedisPoolConfig.setBlockWhenExhausted(true);
+        jedisPoolConfig.setJmxEnabled(true);
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port, timeout, password);
+        return jedisPool;
+
+    }
+}
