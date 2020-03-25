9eca7a9e2c3a269a3eeee2db5bbb57b035cc7c41
sch <1208579668@qq.com>
2020-03-25 20:11:18 +0800

    DONE 用户删除,修改,查询指定地址

--- api/front-web/src/main/java/com/sch/frontweb/controller/AddressController.java
+++ api/front-web/src/main/java/com/sch/frontweb/controller/AddressController.java
@@ -5,12 +5,16 @@ import com.sch.commonbasic.VO.Result;
 import com.sch.commonbasic.enums.ResultEnum;
 import com.sch.frontweb.config.RedisUtil;
 import com.sch.userbase.AO.AddressAO;
+import com.sch.userbase.VO.AddressVO;
+import com.sch.userbase.VO.InsertAddressVO;
 import com.sch.userbase.base.AddressBaseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 /**
  * @Description: 用户地址
@@ -43,12 +47,17 @@ public class AddressController {
      * @param addressAO 新的地址
      */
     @PostMapping("/insert")
-    public Result AddAddress(@RequestBody AddressAO addressAO) {
+    public Result AddAddress(@RequestBody @Validated AddressAO addressAO) {
         Long userId = (Long) session.getAttribute(request.getHeader("Authorization"));
         addressAO.setUserId(userId);
-        addressBaseService.addAddress(addressAO);
-        redisUtil.setnx(DEFAULT_ADDRESS + userId, addressAO.getDetailedAddress());
-        return new Result(ResultEnum.ADD_SUCCESS, redisUtil.get(DEFAULT_ADDRESS + userId, INDEX_DB));
+        Long id = addressBaseService.addAddress(addressAO);
+
+        redisUtil.setnx(DEFAULT_ADDRESS + userId, Long.toString(id));
+
+        InsertAddressVO vo = new InsertAddressVO();
+        vo.setId(id);
+        vo.setDefaultId(Long.valueOf(redisUtil.get(DEFAULT_ADDRESS + userId, INDEX_DB)));
+        return new Result(ResultEnum.ADD_SUCCESS, vo);
     }
 
     /**
@@ -56,7 +65,7 @@ public class AddressController {
      *
      * @return 地址列表
      */
-    @GetMapping("/list")
+    @PostMapping("/list")
     public Result findAddress() {
         Long userId = (Long) session.getAttribute(request.getHeader("Authorization"));
         return Result.success(addressBaseService.findAddressList(userId));
@@ -67,9 +76,47 @@ public class AddressController {
      *
      * @return 默认详细地址
      */
-    @GetMapping("/default")
+    @PostMapping("/default")
     public Result findDefaultAddress() {
         Long userId = (Long) session.getAttribute(request.getHeader("Authorization"));
-        return Result.success(redisUtil.get(DEFAULT_ADDRESS + userId, INDEX_DB));
+        AddressVO addressVO = addressBaseService.findAddressById(Long.valueOf(redisUtil.get(DEFAULT_ADDRESS + userId, INDEX_DB)));
+        return Result.success(addressVO);
+    }
+
+    /**
+     * 用户修改默认地址
+     *
+     * @param map 新默认地址id
+     */
+    @PutMapping("/default")
+    public Result updateDefaultAddress(@RequestBody Map<String, String> map) {
+        Long userId = (Long) session.getAttribute(request.getHeader("Authorization"));
+        // 删除旧的默认地址
+        redisUtil.del(DEFAULT_ADDRESS + userId);
+        // 设置新的默认地址
+        redisUtil.setnx(DEFAULT_ADDRESS + userId, map.get("id"));
+        return Result.success();
+    }
+
+    /**
+     * 用户删除地址
+     *
+     * @param id 被删除的地址id
+     */
+    @DeleteMapping("/delete")
+    public Result deleteAddress(@RequestParam Long id) {
+        addressBaseService.deleteAddressById(id);
+        return Result.success();
+    }
+
+    /**
+     * 用户修改地址
+     *
+     * @param addressAO 修改后的地址信息
+     */
+    @PutMapping("/update")
+    public Result updateAddress(@RequestBody @Validated AddressAO addressAO) {
+        addressBaseService.updateAddressById(addressAO);
+        return new Result(ResultEnum.UPDATE_SUCCESS);
     }
 }
--- api/front-web/src/test/java/com/sch/frontweb/FrontWebApplicationTests.java
+++ api/front-web/src/test/java/com/sch/frontweb/FrontWebApplicationTests.java
@@ -1,6 +1,5 @@
 package com.sch.frontweb;
 
-import com.sch.commonbasic.util.SnowUtil;
 import com.sch.frontweb.config.RedisUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +12,7 @@ class FrontWebApplicationTests {
 
     @Test
     void contextLoads() {
-        redisUtil.setnx("12", "12");
-        System.out.println(redisUtil.get("12", 0));
-        redisUtil.del("12");
-        redisUtil.setnx("12", "13");
-        System.out.println(redisUtil.get("12", 0));
-        System.out.println(SnowUtil.nextId());
+        System.out.println(Long.valueOf(redisUtil.get("DEFAULT_ADDRESS_1", 0)));
     }
 
 }
